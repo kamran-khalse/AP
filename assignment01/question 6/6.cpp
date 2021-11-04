49f9badd7d63e44676147fe0acf82c0f7eeb16c1
Alirez1043 <92548432+Alirez1043@users.noreply.github.com>
2021-11-05 00:40:13 +0330

    Update 6.cpp

--- assignment01/question 6/6.cpp
+++ assignment01/question 6/6.cpp
@@ -1,7 +1,7 @@
 
-bool palindrome(string str)
+bool isPalindrome(string str)
 {
     if( (str.size() <= 1) ) { return true; }
     if( (str[0] != str[str.size()-1]) ) { return false; }
-    return palindrome(str.substr(1,str.size()-2));
+    return isPalindrome(str.substr(1,str.size()-2));
 }
