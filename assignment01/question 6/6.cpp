3d4b7096c8659260371766cd384198a66acb5422
Alirez1043 <92548432+Alirez1043@users.noreply.github.com>
2021-11-04 22:49:23 +0330

    Add files via upload

---
+++ assignment01/question 6/6.cpp
@@ -0,0 +1,7 @@
+
+bool palindrome(string str)
+{
+    if( (str.size() <= 1) ) { return true; }
+    if( (str[0] != str[str.size()-1]) ) { return false; }
+    return palindrome(str.substr(1,str.size()-2));
+}
